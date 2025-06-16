b3b873bc8d45e5adef447f129c2c76954e48a9fd
Antonio de Jesus <antonio719@hotmail.com>
2025-06-15 20:18:47 -0600

    Se modificaron los archivos para controlar los modelos correspondientes

--- backend/models/carrera.js
+++ backend/models/carrera.js
@@ -1,1 +1,9 @@
-js
+const mongoose = require('mongoose');
+
+const CarreraSchema = new mongoose.Schema({
+nombre: String,
+clave: String,
+planesEstudio: [{ type: mongoose.Schema.Types.ObjectId, ref: 'PlanEstudio' }]
+});
+
+module.exports = mongoose.model('Carrera', CarreraSchema);
--- backend/models/profesor.js
+++ backend/models/profesor.js
@@ -1,1 +1,10 @@
-js
+const mongoose = require('mongoose');
+
+const ProfesorSchema = new mongoose.Schema({
+nombre: String,
+apellido: String,
+correo: String,
+carrera: { type: mongoose.Schema.Types.ObjectId, ref: 'Carrera' },
+});
+
+module.exports = mongoose.model('Profesor', ProfesorSchema);
