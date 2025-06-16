609e9f25a814107b06b1b122844a64ef5a7d3d3e
Antonio de Jesus <antonio719@hotmail.com>
2025-06-15 20:22:54 -0600

    Se modifico el controlador del CRUD de los profesores

--- backend/controllers/profesorController.js
+++ backend/controllers/profesorController.js
@@ -1,1 +1,32 @@
-html
+const Profesor = require('../models/Profesor');
+const Evaluacion = require('../models/EvaluacionProfesor');
+
+exports.create = async (req, res) => {
+const nuevo = new Profesor(req.body);
+const guardado = await nuevo.save();
+res.json(guardado);
+};
+
+exports.read = async (req, res) => {
+const lista = await Profesor.find().populate('carrera');
+res.json(lista);
+};
+
+exports.update = async (req, res) => {
+const actualizado = await Profesor.findByIdAndUpdate(req.params.id, req.body, { new: true });
+res.json(actualizado);
+};
+
+exports.delete = async (req, res) => {
+await Profesor.findByIdAndDelete(req.params.id);
+res.sendStatus(204);
+};
+
+exports.evaluarProfesor = async (req, res) => {
+const evaluacion = new Evaluacion({
+    profesor: req.params.id,
+    ...req.body
+});
+const guardada = await evaluacion.save();
+res.json(guardada);
+};
