971b13867c49c3f3783edfd4be55315410bac8f4
Antonio de Jesus <antonio719@hotmail.com>
2025-06-15 20:25:35 -0600

    Se modifico el controlador del CRUD de los postgrados

--- backend/controllers/postgradoController.js
+++ backend/controllers/postgradoController.js
@@ -1,1 +1,18 @@
-js
+const Postgrado = require('../models/Postgrado');
+
+exports.create = async (req, res) => {
+res.json(await new Postgrado(req.body).save());
+};
+
+exports.read = async (req, res) => {
+res.json(await Postgrado.find().populate('responsable'));
+};
+
+exports.update = async (req, res) => {
+res.json(await Postgrado.findByIdAndUpdate(req.params.id, req.body, { new: true }));
+};
+
+exports.delete = async (req, res) => {
+await Postgrado.findByIdAndDelete(req.params.id);
+res.sendStatus(204);
+};
