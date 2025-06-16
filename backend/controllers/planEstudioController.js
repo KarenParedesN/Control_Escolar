9588fed621d4a024f19f915e07d50515a8781f19
Antonio de Jesus <antonio719@hotmail.com>
2025-06-15 20:24:37 -0600

    Se modifico el controlador del CRUD de los planes de estudio

--- backend/controllers/planEstudioController.js
+++ backend/controllers/planEstudioController.js
@@ -1,1 +1,19 @@
-js
+const Plan = require('../models/PlanEstudio');
+
+exports.create = async (req, res) => {
+const nuevo = new Plan(req.body);
+res.json(await nuevo.save());
+};
+
+exports.read = async (req, res) => {
+res.json(await Plan.find().populate('carrera'));
+};
+
+exports.update = async (req, res) => {
+res.json(await Plan.findByIdAndUpdate(req.params.id, req.body, { new: true }));
+};
+
+exports.delete = async (req, res) => {
+await Plan.findByIdAndDelete(req.params.id);
+res.sendStatus(204);
+};
