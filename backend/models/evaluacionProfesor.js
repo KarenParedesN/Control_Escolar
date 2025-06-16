b84ed709f8492d0cd25dd147941b12fa03f46e13
Antonio de Jesus <antonio719@hotmail.com>
2025-06-15 20:21:00 -0600

    Se modifico el modelo de las evaluaciones de los profesores

--- backend/models/evaluacionProfesor.js
+++ backend/models/evaluacionProfesor.js
@@ -1,1 +1,10 @@
-js
+const mongoose = require('mongoose');
+
+const EvaluacionSchema = new mongoose.Schema({
+profesor: { type: mongoose.Schema.Types.ObjectId, ref: 'Profesor' },
+calificacion: Number,
+comentarios: String,
+fecha: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('EvaluacionProfesor', EvaluacionSchema);
