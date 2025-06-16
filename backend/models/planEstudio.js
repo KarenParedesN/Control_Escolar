6a1d47b1c8a007bd981d97ba7b4c21e411f6d394
Antonio de Jesus <antonio719@hotmail.com>
2025-06-15 20:19:42 -0600

    Se modifico el modelo de los planes de estudio

--- backend/models/planEstudio.js
+++ backend/models/planEstudio.js
@@ -1,1 +1,9 @@
-js
+const mongoose = require('mongoose');
+
+const PlanEstudioSchema = new mongoose.Schema({
+nombre: String,
+descripcion: String,
+carrera: { type: mongoose.Schema.Types.ObjectId, ref: 'Carrera' }
+});
+
+module.exports = mongoose.model('PlanEstudio', PlanEstudioSchema);
