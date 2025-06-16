dc031425f583e4cda42af312e23cec3efca5c705
Antonio de Jesus <antonio719@hotmail.com>
2025-06-15 20:20:20 -0600

    Se modifico el modelo de los postgrados

--- backend/models/postgrado.js
+++ backend/models/postgrado.js
@@ -1,1 +1,9 @@
-js
+const mongoose = require('mongoose');
+
+const PostgradoSchema = new mongoose.Schema({
+nombre: String,
+descripcion: String,
+responsable: { type: mongoose.Schema.Types.ObjectId, ref: 'Profesor' }
+});
+
+module.exports = mongoose.model('Postgrado', PostgradoSchema);
