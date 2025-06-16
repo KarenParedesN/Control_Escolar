53f648d4c5354b7dc30ae55da2ffaf00738a1997
Antonio de Jesus <antonio719@hotmail.com>
2025-06-15 20:26:19 -0600

    Se modifico el controlador de las evaluaciones de los profesores

--- backend/controllers/evaluacionController.js
+++ backend/controllers/evaluacionController.js
@@ -1,1 +1,9 @@
-js
+const Evaluacion = require('../models/EvaluacionProfesor');
+
+exports.create = async (req, res) => {
+res.json(await new Evaluacion(req.body).save());
+};
+
+exports.read = async (req, res) => {
+res.json(await Evaluacion.find().populate('profesor'));
+};
