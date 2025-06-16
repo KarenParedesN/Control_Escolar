afe531a8f5ae5f7e3e9fcf863ad8327a4612dff9
Antonio de Jesus <antonio719@hotmail.com>
2025-06-15 20:23:44 -0600

    Se modifico el controlador del CRUD de las carreas

--- backend/controllers/carreraController.js
+++ backend/controllers/carreraController.js
@@ -1,1 +1,19 @@
-js
+const Carrera = require('../models/Carrera');
+
+exports.create = async (req, res) => {
+const carrera = new Carrera(req.body);
+res.json(await carrera.save());
+};
+
+exports.read = async (req, res) => {
+res.json(await Carrera.find().populate('planesEstudio'));
+};
+
+exports.update = async (req, res) => {
+res.json(await Carrera.findByIdAndUpdate(req.params.id, req.body, { new: true }));
+};
+
+exports.delete = async (req, res) => {
+await Carrera.findByIdAndDelete(req.params.id);
+res.sendStatus(204);
+};
